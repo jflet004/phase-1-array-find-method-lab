d48ce54e99f87c1a7d574578df49f93968a15df6
Jose Fletes <joseangelfletes@gmail.com>
2022-02-06 08:24:05 -0800

    commit tree 4882da3bc29c7504e62a0d73a9c57867868d393a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "W"},
+    //...
+  ]
+
+function superbowlWin(array) {
+  const findWinningRecord = array.find(obj => obj.result === "W");
+  if(findWinningRecord) {
+    return findWinningRecord.year;
+  }
+}
+
+//console.log(superbowlWin(record));
